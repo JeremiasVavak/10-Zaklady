09c5e69304902383c625ab2f9c7d42d62d81d985
JeremiasVavak <jeremi.vavak@post.cz>
2022-08-02 09:29:30 +0200

    Pozdrav

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("JavaScript funguje!");
+
+console.log(Math.floor(Math.random()*100));
+/*
+let mojeBarvaVlasu = prompt("Zadejte svoji barvu vlasů")
+*/
+
+function pozdrav(){
+    console.log("Ahoj")
+}
+
+let rok = prompt("Zadejte rok svého narození");
+let dnesniDatum = new Date();
+pozdrav();
+let stari = dnesniDatum.getFullYear() - rok;
+
+console.log("Uživatel má " + stari + " let");
